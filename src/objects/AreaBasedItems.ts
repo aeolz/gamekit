a4f8063cdf337566ab1ff70794350863a03af98a
Dann <vmakichyan0@gmail.com>
2023-07-26 03:05:55 +0400

    areas

--- src/objects/AreaBasedItems.ts
+++ src/objects/AreaBasedItems.ts
@@ -2,13 +2,14 @@ import { Gettable } from "@aeolz/core/lib/objects/Gettable"
 import { Vector2d, Size, UniversalHitbox, intersection } from ".."
 
 type Constructor<T> = new (...args: any[]) => T
+type ArrayElementType<T> = T extends (infer U)[] ? U : T
 
 export interface Area<T> {
   position: Vector2d
   items: T
 }
 
-export class AreaBasedItems<T extends object> {
+export class AreaBasedItems<T extends { [k: string]: any[] }> {
   areas: Area<T>[] = []
 
   constructor(
@@ -45,10 +46,13 @@ export class AreaBasedItems<T extends object> {
   }
 }
 
-class AreaBasedItemsHandler<T extends object> {
+class AreaBasedItemsHandler<T extends { [k: string]: any[] }> {
   constructor(private itemsGroup: T[]) {}
 
-  get<K extends keyof T>(key: K, uniqueKey: keyof T[K]): T[K][] {
+  get<K extends keyof T>(
+    key: K,
+    uniqueKey: keyof ArrayElementType<T[K]>
+  ): T[K] {
     return this.itemsGroup.reduce((aggr, group) => {
       if (key in group) {
         aggr.push(
@@ -58,10 +62,10 @@ class AreaBasedItemsHandler<T extends object> {
         )
       }
       return aggr
-    }, [] as T[K][])
+    }, [] as T[K])
   }
 
-  add<K extends keyof T>(key: K, ...data: T[K][]): this {
+  add<K extends keyof T>(key: K, ...data: T[K]): this {
     this.itemsGroup.forEach((group) => {
       if (key in group) {
         const arr: any = group[key]
@@ -71,16 +75,17 @@ class AreaBasedItemsHandler<T extends object> {
     return this
   }
 
-  remove<K extends keyof T, UK extends keyof T[K]>(
+  remove<K extends keyof T, UK extends keyof ArrayElementType<T[K]>>(
     key: K,
     uniqueKey: UK,
-    val: T[K][UK]
+    val: ArrayElementType<T[K]>[UK] | ((val: ArrayElementType<T[K]>[UK]) => boolean)
   ): this {
     this.itemsGroup.forEach((group) => {
       if (key in group) {
         // @ts-ignore
         group[key] = (Array.isArray(group[key]) ? group[key] : []).filter(
-          (item) => (uniqueKey in item ? item[uniqueKey] !== val : true)
+          // @ts-ignore
+          (item) => (uniqueKey in item ? (typeof val === 'function' ? val(item[uniqueKey]) : (item[uniqueKey] !== val)) : true)
         )
       }
     })
